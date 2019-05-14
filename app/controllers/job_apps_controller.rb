b649d9fa7692a4d305685da2a11129d6c41721a2
Geoffrey Charles Witherington-Perkins <witherin@msu.edu>
2019-05-14 17:56:36 -0400

    Added new job_app page

--- app/controllers/job_apps_controller.rb
+++ app/controllers/job_apps_controller.rb
@@ -1,11 +1,20 @@
 class JobAppsController < ApplicationController
-	def index
-		@job_apps = JobApp.all
+	def show
+		@job_app = JobApp.find(params[:id])
 	end
 
 	def new
 	end
 
 	def create
+		@job_app = JobApp.new(job_app_params)
+
+		@job_app.save
+		redirect_to @job_app
 	end
+
+	private
+		def job_app_params
+			params.require(:job_app).permit(:company, :status, :url, :notes)
+		end
 end
